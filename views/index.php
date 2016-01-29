8badbc88a89cc91848446cef94332551afaa88fc
isma91 <ismaydogmus@gmail.com>
2016-01-29 17:16:37 +0100

    changing path of the static file like css/js

--- views/index.php
+++ views/index.php
@@ -6,11 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="" />
 	<title></title>
-	<link media="all" type="text/css" rel="stylesheet" href="./../media/css/mui.min.css" />
-	<link media="all" type="text/css" rel="stylesheet" href="./../media/css/materialize.min.css" />
-	<script src="./../media/js/jquery-2.1.4.min.js"></script>
-	<script src="./../media/js/mui.min.js"></script>
-	<script src="./../media/js/materialize.min.js"></script>
+	<link media="all" type="text/css" rel="stylesheet" href="media/css/mui.min.css" />
+	<link media="all" type="text/css" rel="stylesheet" href="media/css/materialize.min.css" />
+	<script src="media/js/jquery-2.1.4.min.js"></script>
+	<script src="media/js/mui.min.js"></script>
+	<script src="media/js/materialize.min.js"></script>
+	<script src="media/js/index.js"></script>
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
