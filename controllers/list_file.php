5ba48f9c52b50c1a30ac34372c02c3dc2fb1f351
isma91 <ismaydogmus@gmail.com>
2016-01-29 17:05:25 +0100

    adding listing file

---
+++ controllers/list_file.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* List_file.php
+*
+* List all file of a directory
+*
+* PHP 5.6.17-0+deb8u1 (cli) (built: Jan 13 2016 09:10:12)
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/display_download/blob/master/controllers/list_file.php
+*/
+ini_set('xdebug.var_display_max_depth', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
+/* take the all path of the directory */
+$directory = '/home/isma91/Téléchargements/';
+//$directory = '$_POST["directory"]';
+$list_directory = scandir($directory);
+$array_file = array();
+$array_folder = array();
+foreach ($list_directory as $file) {
+	if ($file !== ".." && $file !== ".") {
+		if (is_dir($directory.$file)) {
+			array_push($array_folder, $file);
+		} else {
+			array_push($array_file, $file);
+		}
+	}
+}
+$all_array = array('folder' => $array_folder, 'file' => $array_file);
+echo json_encode($all_array);
