3b10471cd0945fc97e662389e67fee54114a248b
isma91 <ismaydogmus@gmail.com>
2016-01-29 17:17:36 +0100

    begin to send some ajax to list_file.php

--- media/js/index.js
+++ media/js/index.js
@@ -2,4 +2,10 @@
 /*jslint devel : true*/
 /*global $, document, this, Materialize*/
 $(document).ready(function () {
+    $.post('controllers/list_file.php', {directory: '/home/isma91/Téléchargements/'}, function (data, textStatus, xhr) {
+        if (textStatus === "success") {
+            data = JSON.parse(data);
+            console.log(data);
+        }
+    });
 });
