887c8c32813268a1c7898433bcd6c011e3572006
isma91 <ismaydogmus@gmail.com>
2016-02-03 16:18:43 +0100

    fix send_path and adding the insertion in a folder

--- media/js/index.js
+++ media/js/index.js
@@ -3,13 +3,15 @@
 /*global $, document, this, Materialize*/
 /* /home/isma91/Téléchargements/ */
 $(document).ready(function () {
-    function send_path () {
+    function send_path (path) {
         "use strict";
-        var path, breadcrumb, folders, files;
-        path = $('#path').val();
+        var breadcrumb, folders, files, file_count;
+        file_count = 0;
         if (path !== "") {
             $.post('controllers/list_file.php', {directory: path}, function (data, textStatus, xhr) {
                 if (textStatus === "success") {
+                    $('#current_path').html(path);
+                    $('#the_body').html('<div class="row mui-panel" id="the_menu"></div>');
                     data = JSON.parse(data);
                     if (data.folder.length === 0 && data.file.length === 0) {
                         Materialize.toast('<p class="alert-failed">No folder or file here !!<p>', 2000, 'rounded alert-failed');
@@ -23,16 +25,14 @@ $(document).ready(function () {
                                 }
                             });
                         }
-                        $('#the_menu').html(' <nav><div class="nav-wrapper"><a href="#" class="brand-logo">Display_Download</a><div class="right">' + breadcrumb + '</div></div></nav>');
+                        $('#the_menu').html(' <nav><div class="nav-wrapper"><a href="#" class="brand-logo">Display_Download</a><div class="right" id="menu_current_path">' + breadcrumb + '</div></div></nav>');
                         $('#the_body').append('<div class="row mui-panel " id="loader"><div class="progress"><div class="indeterminate"></div></div></div>');
                         $('#loader').css({'margin-top': '25%'});
-                        $('#the_menu').html();
                         setTimeout(function () {
                             console.log(data)
                             $('#loader').remove();
-                            $('#the_body').append('<div class="row mui-panel">There is ' + data.folder.length + ' folder(s) and ' + data.file.length + ' file(s)</div>');
+                            folders = '';
                             if (data.folder.length !== 0) {
-                                folders = '';
                                 $.each(data.folder, function (index, folder) {
                                     if (folder === ".git") {
                                         folders = folders + '<div class="folder col s3"><img class="icons" src="media/img/icons/git.png" alt="git folder" /><p class="folder_name">' + folder + '</p></div>';
@@ -43,8 +43,8 @@ $(document).ready(function () {
                                     }
                                 });
                             }
-                            if (data.folder.length !== 0) {
-                                files = '';
+                            files = '';
+                            if (Object.keys(data.file).length !== 0) {
                                 $.each(data.file, function (extension, array_file) {
                                     if (extension === "") {
                                         extension = "txt";
@@ -53,9 +53,11 @@ $(document).ready(function () {
                                     }
                                     $.each(array_file, function (index, file) {
                                         files = files + '<div class="file col s3"><img class="icons" src="media/img/icons/' + extension + '.png" alt="' + extension + ' file" /><p class="file_name">' + file + '</p></div>';
+                                        file_count = file_count + 1;
                                     });
                                 });
                             }
+                            $('#the_body').append('<div class="row mui-panel">There is ' + data.folder.length + ' folder(s) and ' + file_count + ' file(s)</div>');
                             $('#the_body').append('<div class="row"><div class="mui-panel" id="path_content"><div class="row">' + folders + files + '</div></div></div>');
                         }, 1000);
                     }
@@ -67,11 +69,20 @@ $(document).ready(function () {
     }
     $('#path').keypress(function (event){
         if(event.keyCode == 13){
-            send_path();
+            send_path($('#path').val());
             event.preventDefault();
         }
     });
     $('#send_path').click(function () {
-        send_path();
+        send_path($('#path').val());
+    });
+    $(document.body).on('click', '.folder', function () {
+        send_path($('#current_path').text() + '/' + $(this).children('p').text());
+    });
+    $(document.body).on('contextmenu', '.folder', function () {
+        console.log('right click on folder');
+    });
+    $(document.body).on('contextmenu', '.file', function () {
+        console.log('right click on file');
     });
 });
