29db93f52f98f47b98a8f23b02a6406d656d1a47
isma91 <ismaydogmus@gmail.com>
2016-02-08 15:59:08 +0100

    adding empty file system controller

---
+++ controllers/file_system.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* File_system.php
+*
+* Create, copy, delete and many thing
+*
+* PHP 5.6.17-0+deb8u1 (cli) (built: Jan 13 2016 09:10:12)
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/display_download/blob/master/controllers/file_system.php
+*/
+function create_folder ($folder_name, $path) {
+	if (!empty($folder_name) && !empty($path)) {
+	}
+}
+switch ($_POST["action"]) {
+	case 'create_folder':
+		# code...
+	break;
+	case 'copy':
+		# code...
+	break;
+	case 'delete':
+		# code...
+	break;
+	case 'archive':
+		# code...
+	break;
+	default:
+		# code...
+	break;
+}
