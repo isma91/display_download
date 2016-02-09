5731cacec300b1ad002faa5600aea895d6e2206a
isma91 <ismaydogmus@gmail.com>
2016-02-09 11:12:22 +0100

    adding properties of a file

--- controllers/file_system.php
+++ controllers/file_system.php
@@ -26,6 +26,32 @@ function create_folder ($folder_name, $path) {
 		}
 	}
 }
+function get_stat ($file_name, $path) {
+	if (!empty($file_name && !empty($path))) {
+		$file_name_path = realpath($path) . "/" . $file_name;
+		if (file_exists($file_name_path)) {
+			$stat = stat($file_name_path);
+			echo json_encode(array(
+				"device_number" => $stat["dev"],
+				"inode_number" => $stat["ino"],
+				"inode_protection_mode" => $stat["mode"],
+				"nb_links" => $stat["nlinks"],
+				"size" => $stat["size"],
+				"user_owner" => posix_getpwuid($stat["uid"]),
+				"gruop_owner" => posix_getgrgid($stat["gid"]),
+				"device_type" => $stat["rdev"],
+				"file_last_access" => date("F d Y H:i:s.", $stat["atime"]),
+				"file_last_modification" => date("F d Y H:i:s.", $stat["mtime"]),
+				"file_last_changing_right" => date("F d Y H:i:s.", $stat["ctime"]),
+				"block_size" => $stat["blksize"],
+				"num_512bits_blick_allowed" => $stat["blocks"],
+				)
+			);
+		} else {
+			echo json_encode(array('error' => "The file " . $file_name . "  don't exists !!", 'data' =>  null));
+		}
+	}
+}
 switch ($_POST["action"]) {
 	case 'create_folder':
 	create_folder($_POST["name"], rtrim($_POST["to"], '/') . '/');
@@ -35,6 +61,9 @@ switch ($_POST["action"]) {
 	case 'delete':
 	break;
 	case 'archive':
+	break;
+	case 'properties':
+	get_stat($_POST["name"], rtrim($_POST["to"], '/') . '/');
 	break;
 	default:
 	break;
