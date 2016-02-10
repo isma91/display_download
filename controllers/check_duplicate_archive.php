281012dc9dd05c334d9276ac746d9b8265f6de52
isma91 <ismaydogmus@gmail.com>
2016-02-10 10:42:56 +0100

    php file who check if archive name is already taken

---
+++ controllers/check_duplicate_archive.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* List_file.php
+*
+* List all file of a directory
+*
+* PHP 5.6.17-0+deb8u1 (cli) (built: Jan 13 2016 09:10:12)
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/display_download/blob/master/controllers/list_file.php
+*/
+$archive_path = realpath(rtrim($_POST["path"], "/") . "/" . $_POST["archive_name"] . $_POST["extension"]);
+echo json_encode(array('file_exists' => file_exists($archive_path)));
