1ed95b4a142cf7ddd8186cda5dac36e614123e07
isma91 <ismaydogmus@gmail.com>
2016-02-12 14:59:45 +0100

    adding rar extraction

--- controllers/file_system.php
+++ controllers/file_system.php
@@ -12,6 +12,9 @@
 * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
 * @link     https://github.com/isma91/display_download/blob/master/controllers/file_system.php
 */
+ini_set('xdebug.var_display_max_depth', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
 function send_json ($error, $data) {
 	echo json_encode(array('error' => $error, 'data' => $data));
 }
@@ -102,17 +105,6 @@ function create_archive ($archive_name, $path, $extension, $files) {
 				array_push($array_path_folders, $path . $file);
 			}
 		}
-		foreach ($array_path_folders as $folder) {
-			$list_folder = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder));
-			foreach ($list_folder as $file_in_folder) {
-				if ($file_in_folder->isDir()) {
-					continue;
-				}
-				array_push($array_path_files, $file_in_folder->getPathname());
-				array_push($array_files, substr($file_in_folder->getPathname(), strlen($path)));
-			}
-		}
-		$array_all = array_combine($array_path_files, $array_files);
 		if ($extension === ".zip") {
 			$tmp_archive = tempnam("tmp", "zip");
 			$archive = new ZipArchive();
@@ -122,31 +114,150 @@ function create_archive ($archive_name, $path, $extension, $files) {
 				send_json("Can't create zip archive !!", null);
 				throw new Exception("Can't create zip archive !!");
 			}
+			foreach ($array_path_folders as $folder) {
+				$list_folder = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder));
+				foreach ($list_folder as $file_in_folder) {
+					if ($file_in_folder->isDir()) {
+						continue;
+					}
+					array_push($array_path_files, $file_in_folder->getPathname());
+					array_push($array_files, substr($file_in_folder->getPathname(), strlen($path)));
+				}
+			}
+			$array_all = array_combine($array_path_files, $array_files);
+			foreach ($array_all as $full_path => $file_name) {
+				if (file_exists($full_path)) {
+					if (is_readable($full_path)) {
+						$archive->addFile($full_path, $file_name);
+					}
+				}
+			}
+			$archive->close();
 		} else {
 			$tmp_archive = tempnam("tmp", "tar") . ".tar";
 			$archive = new PharData($tmp_archive);
-		}
-		foreach ($array_all as $full_path => $file_name) {
-			if (file_exists($full_path)) {
-				if (is_readable($full_path)) {
+			if (count($array_path_folders) > 0) {
+				$tmp_archive = tempnam("tmp", "tar") . ".tar";
+				$archive = new PharData($tmp_archive);
+				foreach ($array_path_folders as $folder) {
+					try {
+						$archive->buildFromDirectory($folder);
+					} catch (Exception $e) {
+						send_json($e->getMessage(), null);
+						throw new Exception($e->getMessage());
+						
+					}
+				}
+			}
+			if (count($array_path_files) > 0 && count($array_files) > 0) {
+				$array_all = array_combine($array_path_files, $array_files);
+				foreach ($array_all as $full_path => $file_name) {
 					$archive->addFile($full_path, $file_name);
 				}
 			}
 		}
-		if ($extension === ".zip") {
-			$archive->close();
-		} elseif ($extension !== ".tar") {
-			$archive_type_compress = substr($extension, 1);
+		if ($extension !== ".tar" && $extension !== ".zip") {
+			$archive_type_compress = substr(strrchr($extension, "."), 1);
 			if ($archive_type_compress == "gz") {
 				$archive->compress(Phar::GZ);
-				$extension = ".tar.gz";
 			} elseif ($archive_type_compress == "bz2") {
 				$archive->compress(Phar::BZ2);
-				$extension = ".tar.bz2";
+			}
+			$tmp_archive = $tmp_archive . "." . $archive_type_compress;
+		}
+		rename($tmp_archive, $path . $archive_name . $extension);
+		/*if (file_exists($tmp_archive)) {
+			unlink($tmp_archive);
+		}*/
+		send_json(null, null);
+	}
+}
+function extract_archive ($archive_name, $path, $password) {
+	if (!empty($archive_name) && !empty($path)) {
+		$extension = pathinfo($path . $archive_name, PATHINFO_EXTENSION);
+		$array_extension = array("tar" => 4, "gz" => 7, "bz2" => 8, "zip" => 4, "rar" => 4);
+		$accepted_extension = false;
+		$strlen_extension = "";
+		foreach ($array_extension as $archive_extension => $strlen_archive_extension) {
+			if ($extension === $archive_extension) {
+				$accepted_extension = true;
+				$strlen_extension = $strlen_archive_extension;
+				break;
 			}
 		}
-		send_json(null, null);
-		rename($tmp_archive, $path . $archive_name . $extension);
+		if ($accepted_extension === false) {
+			send_json('Extension not supported !!', null);
+			throw new Exception("Extension not supported !!");
+		}
+		$only_file_name = substr($archive_name, 0, -$strlen_extension);
+		$last_pos_only_file_name =  strrpos($only_file_name, "-");
+		$tmp_only_file_name = substr($only_file_name, 0, $last_pos_only_file_name);
+		$counter_duplicate = substr($only_file_name, $last_pos_only_file_name + 1);
+		if (!is_int($last_pos_only_file_name) || !is_int($counter_duplicate)) {
+			$tmp_only_file_name = $only_file_name;
+			$counter_duplicate = 1;
+		}
+		while (file_exists($path . $only_file_name)) {
+			$only_file_name = $tmp_only_file_name . "-" . $counter_duplicate;
+			$counter_duplicate++;
+		}
+		mkdir($path . $only_file_name);
+		$a = 0;
+		if ($extension === "zip") {
+			try {
+				$zip = zip_open($path . $archive_name);
+				if ($zip) {
+					while ($zip_read = zip_read($zip)) {
+						$zip_entry_name = zip_entry_name($zip_read);
+						if (zip_entry_open($zip, $zip_read)) {
+							$file_in_archive =  zip_entry_read($zip_read, zip_entry_filesize($zip_read));
+							$exploded = explode("/", substr($path . $only_file_name . "/" . zip_entry_name($zip_read), strlen($path . $only_file_name) + 1));
+							for ($i = 0; $i < count($exploded) - 1; $i++) {
+								if ($i === 0) {
+									$path_after_onyl_file_name = $exploded[0];
+								} else {
+									$path_after_onyl_file_name = $path_after_onyl_file_name . "/" . $exploded[$i];
+								}
+								if (!file_exists($path . $only_file_name . "/" . $path_after_onyl_file_name)) {
+									mkdir($path . $only_file_name . "/" . $path_after_onyl_file_name, 0777, true);
+								}
+							}
+							file_put_contents($path . $only_file_name . "/" . zip_entry_name($zip_read), $file_in_archive);
+						}
+					}
+					zip_close($zip);
+				}
+			} catch (Exception $e) {
+				send_json($e->getMessage(), null);
+				throw new Exception($e->getMessage());
+			}
+		} elseif ($extension === "rar") {
+			if ($password === "") {
+				$rar = RarArchive::open($path . $archive_name);
+			} else {
+				$rar = RarArchive::open($path . $archive_name, $password);
+			}
+			if ($rar->isBroken()) {
+				send_json("Rar archive broken !!", null);
+				throw new Exception("Rar archive broken !!");
+			}
+			$rar_file = rar_open($path . $archive_name);
+			$list = rar_list($rar_file);
+			foreach($list as $file) {
+				$entry = rar_entry_get($rar_file, $file->getName());
+				$entry->extract($path . $only_file_name);
+			}
+			rar_close($rar_file);
+		} else {
+			$archive = new PharData($path . $archive_name);
+			try {
+				$archive->extractTo($path . $only_file_name);
+			} catch (Exception $e) {
+				send_json($e->getMessage(), null);
+				throw new Exception($e->getMessage());
+			}
+		}
+		send_json(null, $only_file_name);
 	}
 }
 switch ($_POST["action"]) {
@@ -159,6 +270,9 @@ switch ($_POST["action"]) {
 	break;
 	case 'create_archive':
 	create_archive($_POST["archive_name"], rtrim($_POST["to"], '/') . '/', $_POST["extension"], $_POST["files"]);
+	break;
+	case 'extract_archive':
+	extract_archive($_POST["archive_name"], rtrim($_POST["to"], '/') . '/', $_POST["archive_password"]);
 	break;
 	case 'properties':
 	get_stat($_POST["name"], rtrim($_POST["to"], '/') . '/');
